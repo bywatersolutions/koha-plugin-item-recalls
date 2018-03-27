803779d395e84c3d1d551ad8304b927c565709b7
Kyle M Hall <kyle@bywatersolutions.com>
2018-03-27 02:06:12 +0000

    Add ability to perform auto-recalls on all recallable holds

--- Koha/Plugin/Com/ByWaterSolutions/ItemRecalls.pm
+++ Koha/Plugin/Com/ByWaterSolutions/ItemRecalls.pm
@@ -24,9 +24,9 @@ BEGIN {
     use Config;
     use C4::Context;
 
-    my $pluginsdir = C4::Context->config('pluginsdir');
+    my $pluginsdir  = C4::Context->config('pluginsdir');
     my $plugin_libs = '/Koha/Plugin/Com/ByWaterSolutions/ItemRecalls/lib/perl5';
-    my $local_libs = "$pluginsdir/$plugin_libs";
+    my $local_libs  = "$pluginsdir/$plugin_libs";
 
     unshift( @INC, $local_libs );
     unshift( @INC, "$local_libs/$Config{archname}" );
@@ -70,12 +70,13 @@ sub configure {
     unless ( $cgi->param('save') ) {
         my $template = $self->get_template( { file => 'configure.tt' } );
 
-        my $query = q{SELECT * FROM plugin_data WHERE plugin_class = 'Koha::Plugin::Com::ByWaterSolutions::ItemRecalls'};
-        my $sth = $dbh->prepare( $query );
+        my $query =
+q{SELECT * FROM plugin_data WHERE plugin_class = 'Koha::Plugin::Com::ByWaterSolutions::ItemRecalls'};
+        my $sth = $dbh->prepare($query);
         $sth->execute();
         my $data;
         while ( my $r = $sth->fetchrow_hashref ) {
-            $data->{ $r->{plugin_key} } = $r->{plugin_value}
+            $data->{ $r->{plugin_key} } = $r->{plugin_value};
         }
 
         $template->param(%$data);
@@ -91,12 +92,14 @@ sub configure {
     }
     else {
         my $data = { $cgi->Vars };
-        delete $data->{ $_ } for qw( method save class );
+        delete $data->{$_} for qw( method save class );
 
         $self->update_syspref( 'opacuserjs',     $data );
         $self->update_syspref( 'intranetuserjs', $data );
 
-        $dbh->do(q{DELETE FROM plugin_data WHERE plugin_key LIKE "enable%" AND plugin_class = 'Koha::Plugin::Com::ByWaterSolutions::ItemRecalls'});
+        $dbh->do(
+q{DELETE FROM plugin_data WHERE plugin_key LIKE "enable%" AND plugin_class = 'Koha::Plugin::Com::ByWaterSolutions::ItemRecalls'}
+        );
         $self->store_data($data);
 
         $self->go_home();
@@ -108,87 +111,22 @@ sub api {
     my $cgi = $self->{cgi};
 
     my $reserve_id = $cgi->param('reserve_id');
-    my $action = $cgi->param('action');
+    my $action     = $cgi->param('action');
 
     my $rules = $self->retrieve_data('recall_rules') . "\n";
-    $rules = YAML::Load( $rules );
+    $rules = YAML::Load($rules);
 
-    my $hold = Koha::Holds->find( $reserve_id );
+    my $hold = Koha::Holds->find($reserve_id);
 
-    my $dbh = C4::Context->dbh;
-    my $sth = $dbh->prepare("SELECT * FROM plugin_recalls WHERE reserve_id = ?");
-    $sth->execute( $hold->id );
-    my $recall = $sth->fetchrow_hashref;
-
-    my $rule;
-
-    if ( !$recall && $hold && $hold->itemnumber && $hold->item->checkout && $hold->priority eq '1' ) { # Recalls only work for item level holds
-        my $item = $hold->item;
-        foreach my $r (@$rules) {
-            my $it_match =  !$r->{itemtype} || $r->{itemtype} eq $item->itype;
-            my $cc_match =  !$r->{categorycode} || $r->{categorycode} eq $item->ccode;
-            my $bc_match =  !$r->{branchcode} || $r->{branchcode} eq $hold->branchcode;
-
-            if ( $it_match && $cc_match && $bc_match ) {
-                $rule = $r;
-                last;
-            }
-        }
-    }
-
-    my $can_recall = $rule ? 1 : 0;
+    my $can_recall_rule = $self->can_recall( $hold, $rules );
 
     my $data;
     if ( $action eq 'can_item_be_recalled' ) {
-        $data->{can_recall} = $can_recall;
+        $data->{can_recall} = $can_recall_rule;
     }
     elsif ( $action eq 'recall_item' ) {
-        if ($can_recall) {
-            my $item     = $hold->item;
-            my $checkout = $item->checkout;
-            my $patron   = $checkout->patron;
-
-            my $date_due = dt_from_string( $checkout->date_due );
-            my $new_date_due =
-              dt_from_string()->add( days => $rule->{due_date_length} );
-
-          # Don't update date due if it is already due soon then date_due_length
-            if ( $date_due > $new_date_due ) {
-                $checkout->date_due($new_date_due);
-                $checkout->store() or $data->{warning} = 'Unable to reduce due date';
-            }
-
-            $dbh->do(qq{
-                INSERT INTO plugin_recalls ( issue_id, reserve_id, rule ) VALUES ( ?, ?, ? );
-            }, undef, ( $checkout->id, $hold->id, YAML::Dump($rule) ) );
-
-            my $letter = C4::Letters::GetPreparedLetter(
-                module      => 'reserves',
-                letter_code => 'RECALL_PLUGIN',
-                branchcode  => $hold->branchcode,
-                lang        => $patron->lang,
-                tables      => {
-                    branches  => $checkout->branchcode,
-                    borrowers => $patron->id,
-                    biblio    => $item->biblio->id,
-                    items     => $item->id,
-                    issues    => $item->id,
-                },
-                substitute => {
-                    rule => $rule,
-                },
-            );
-            if ($letter) {
-                C4::Letters::EnqueueLetter(
-                    {
-                        letter         => $letter,
-                        borrowernumber => $patron->id,
-                        message_transport_type => 'email',
-                    }
-                ) or $data->{warning} = 'Unable to send email to current borrower';
-            }
-
-            $data->{success} = 1;
+        if ($can_recall_rule) {
+            $data = $self->recall_item($hold, $can_recall_rule);
         }
         else {
             $data->{success} = 0;
@@ -205,6 +143,99 @@ sub api {
     print to_json($data);
 }
 
+sub can_recall {
+    my ( $self, $hold, $rules ) = @_;
+
+    my $dbh = C4::Context->dbh;
+    my $sth =
+      $dbh->prepare("SELECT * FROM plugin_recalls WHERE reserve_id = ?");
+    $sth->execute( $hold->reserve_id );
+    my $recall = $sth->fetchrow_hashref;
+
+    my $rule;
+
+    if (  !$recall
+        && $hold
+        && $hold->itemnumber
+        && $hold->item->checkout
+        && $hold->priority eq '1' )
+    {    # Recalls only work for item level holds
+        my $item = $hold->item;
+        foreach my $r (@$rules) {
+            my $it_match = !$r->{itemtype} || $r->{itemtype} eq $item->itype;
+            my $cc_match =
+              !$r->{categorycode} || $r->{categorycode} eq $item->ccode;
+            my $bc_match =
+              !$r->{branchcode} || $r->{branchcode} eq $hold->branchcode;
+
+            if ( $it_match && $cc_match && $bc_match ) {
+                $rule = $r;
+                last;
+            }
+        }
+    }
+
+    return $rule;
+}
+
+sub recall_item {
+    my ($self, $hold, $rule) = @_;
+
+    my $item     = $hold->item;
+    my $checkout = $item->checkout;
+    my $patron   = $checkout->patron;
+
+    my $dbh = C4::Context->dbh;
+
+    my $data;
+
+    my $date_due = dt_from_string( $checkout->date_due );
+    my $new_date_due =
+      dt_from_string()->add( days => $rule->{due_date_length} );
+
+    # Don't update date due if it is already due soon then date_due_length
+    if ( $date_due > $new_date_due ) {
+        $checkout->date_due($new_date_due);
+        $checkout->store() or $data->{warning} = 'Unable to reduce due date';
+    }
+
+    $dbh->do(
+        qq{
+                INSERT INTO plugin_recalls ( issue_id, reserve_id, rule ) VALUES ( ?, ?, ? );
+            }, undef, ( $checkout->id, $hold->id, YAML::Dump($rule) )
+    );
+
+    my $letter = C4::Letters::GetPreparedLetter(
+        module      => 'reserves',
+        letter_code => 'RECALL_PLUGIN',
+        branchcode  => $hold->branchcode,
+        lang        => $patron->lang,
+        tables      => {
+            branches  => $checkout->branchcode,
+            borrowers => $patron->id,
+            biblio    => $item->biblio->id,
+            items     => $item->id,
+            issues    => $item->id,
+        },
+        substitute => {
+            rule => $rule,
+        },
+    );
+    if ($letter) {
+        C4::Letters::EnqueueLetter(
+            {
+                letter                 => $letter,
+                borrowernumber         => $patron->id,
+                message_transport_type => 'email',
+            }
+        ) or $data->{warning} = 'Unable to send email to current borrower';
+    }
+
+    $data->{success} = 1;
+
+    return $data;
+}
+
 sub cronjob_nightly {
     my ( $self, $args ) = @_;
 
@@ -214,25 +245,34 @@ sub cronjob_nightly {
         'SELECT * FROM plugin_recalls WHERE issue_id IS NOT NULL',
         { Slice => {} } );
 
-    foreach my $r ( @$recalls ) {
+    foreach my $r (@$recalls) {
         my $checkout = Koha::Checkouts->find( $r->{issue_id} );
         if ( $checkout->is_overdue ) {
             my $rule = YAML::Load( $r->{rule} );
 
             if ( $rule->{past_due_restrict} ) {
-                my $title = $checkout->item->biblio->title;
+                my $title   = $checkout->item->biblio->title;
                 my $barcode = $checkout->item->barcode;
-                my $date_due_formatted = Koha::DateUtils::format_sqldatetime( $checkout->date_due );
+                my $date_due_formatted =
+                  Koha::DateUtils::format_sqldatetime( $checkout->date_due );
 
-                my $comment = "Patron restricted for failing to return recalled item in time: $title ( $barcode ) Due $date_due_formatted";
+                my $comment =
+                    "Patron restricted for failing to "
+                  . "return recalled item in time: "
+                  . "$title ( $barcode ) Due $date_due_formatted";
 
-                my $restrictions = Koha::Patron::Debarments::GetDebarments( { borrowernumber => $checkout->patron->id, comment => $comment } );
+                my $restrictions = Koha::Patron::Debarments::GetDebarments(
+                    {
+                        borrowernumber => $checkout->patron->id,
+                        comment        => $comment
+                    }
+                );
                 unless (@$restrictions) {
                     Koha::Patron::Debarments::AddDebarment(
                         {
                             borrowernumber => $checkout->patron->id,
-                            type => 'MANUAL',
-                            comment => $comment,
+                            type           => 'MANUAL',
+                            comment        => $comment,
                         }
                     );
                 }
@@ -240,15 +280,26 @@ sub cronjob_nightly {
 
             if ( $rule->{past_due_fine_amount} ) {
                 my $barcode = $checkout->item->barcode;
-                my $date_due_formatted = Koha::DateUtils::format_sqldatetime( $checkout->date_due );
+                my $date_due_formatted =
+                  Koha::DateUtils::format_sqldatetime( $checkout->date_due );
 
-                my $description = "Failure to return recalled item in time: ( $barcode ) Due $date_due_formatted";
+                my $description =
+                    "Failure to return recalled item in time: "
+                  . "( $barcode ) Due $date_due_formatted";
 
                 my $recalls = $dbh->selectall_arrayref(
-                    'SELECT * FROM accountlines WHERE borrowernumber = ? AND itemnumber = ? AND description = ? AND accounttype = ?',
-                    { Slice => {} }, $checkout->patron->id, $checkout->item->id, $description, 'F' );
-                unless ( @$recalls ) {
-                    C4::Accounts::manualinvoice( $checkout->patron->id, $checkout->item->id, $description, 'F', $rule->{past_due_fine_amount} );
+                    'SELECT * FROM accountlines WHERE borrowernumber = ?'
+                      . 'AND itemnumber = ? AND description = ? AND accounttype = ?',
+                    { Slice => {} },
+                    $checkout->patron->id,
+                    $checkout->item->id,
+                    $description,
+                    'F'
+                );
+                unless (@$recalls) {
+                    C4::Accounts::manualinvoice( $checkout->patron->id,
+                        $checkout->item->id, $description, 'F',
+                        $rule->{past_due_fine_amount} );
                 }
             }
         }
@@ -260,19 +311,45 @@ sub cronjob {
 
     my $dbh = C4::Context->dbh;
 
+    # Auto recall any checkouts with holds
+    if ( $self->retrieve_data('enable_auto_recall') ) {
+        my $rules = $self->retrieve_data('recall_rules') . "\n";
+        $rules = YAML::Load($rules);
+
+        # This query replicates the can_recall logic to an extent for speed
+        my $unrecalled = $dbh->selectall_arrayref(
+            q{
+                SELECT reserve_id FROM reserves
+                  LEFT JOIN issues USING ( itemnumber )
+                  LEFT JOIN plugin_recalls USING ( reserve_id )
+                 WHERE reserves.itemnumber IS NOT NULL
+                   AND reserves.priority = '1'
+                   AND issues.issue_id IS NOT NULL
+                   AND plugin_recalls.reserve_id IS NULL
+            }, { Slice => {} }
+        );
+
+        foreach my $u (@$unrecalled) {
+            my $hold = Koha::Holds->find( $u->{reserve_id} );
+            my $rule = $self->can_recall( $hold, $rules );
+            $self->recall_item($hold, $rule) if $rule;
+        }
+    }
+
     my $recalls = $dbh->selectall_arrayref(
         'SELECT * FROM plugin_recalls WHERE issue_id IS NULL',
         { Slice => {} } );
 
-    for my $r ( @$recalls ) {
+    for my $r (@$recalls) {
         my $hold = Koha::Holds->find( $r->{reserve_id} );
 
         next if $hold->notificationdate;
 
         my $rule = YAML::Load( $r->{rule} );
 
-        my $expirationdate = dt_from_string( $hold->waitingdate )->add( days => $rule->{pickup_date_length} );
-        $hold->expirationdate( $expirationdate );
+        my $expirationdate = dt_from_string( $hold->waitingdate )
+          ->add( days => $rule->{pickup_date_length} );
+        $hold->expirationdate($expirationdate);
         $hold->notificationdate( dt_from_string() );
         $hold->store();
 
@@ -285,9 +362,9 @@ sub cronjob {
             }
         );
 
-        my $patron = Koha::Patrons->find( $hold->borrowernumber );
+        my $patron  = Koha::Patrons->find( $hold->borrowernumber );
         my $library = Koha::Libraries->find( $hold->branchcode );
-        my $letter = C4::Letters::GetPreparedLetter(
+        my $letter  = C4::Letters::GetPreparedLetter(
             module      => 'reserves',
             letter_code => 'RECALL_PICKUP_PLUGIN',
             branchcode  => $hold->branchcode,
@@ -308,8 +385,8 @@ sub cronjob {
         if ($letter) {
             my $id = C4::Letters::EnqueueLetter(
                 {
-                    letter         => $letter,
-                    borrowernumber => $patron->id,
+                    letter                 => $letter,
+                    borrowernumber         => $patron->id,
                     message_transport_type => 'email',
                 }
             );
@@ -330,7 +407,8 @@ sub install() {
 
     my $dbh = C4::Context->dbh;
 
-    $dbh->do(qq{
+    $dbh->do(
+        qq{
         CREATE TABLE IF NOT EXISTS `plugin_recalls` (
           `issue_id` INT(11) NULL,
           `reserve_id` INT(11) NOT NULL,
@@ -340,29 +418,35 @@ sub install() {
           CONSTRAINT `plugin_recalls_ibfk_2` FOREIGN KEY (`reserve_id`) REFERENCES `reserves` (`reserve_id`) ON DELETE CASCADE ON UPDATE CASCADE,
           CONSTRAINT `plugin_recalls_ibfk_1` FOREIGN KEY (`issue_id`) REFERENCES `issues` (`issue_id`) ON DELETE SET NULL ON UPDATE CASCADE
         ) ENGINE=InnoDB;
-    });
+    }
+    );
 
-    $dbh->do(q{
+    $dbh->do(
+        q{
         INSERT IGNORE INTO `letter` (`module`, `code`, `branchcode`, `name`, `is_html`, `title`, `content`, `message_transport_type`, `lang`)
         VALUES
             ('reserves', 'RECALL_PLUGIN', '', 'Recall Notice for Recalls Plugin', 0, 'An item you have checked out has been recalled', '[%- USE KohaDates -%]\r\n[%- USE Price -%]\r\nDate: <<today>>\r\n\r\nA recall has been placed on the following item: [% biblio.title %] / [% biblio.author %] ( [% item.barcode %] ).\r\nThe due date has been updated, and is now [% checkout.date_due | $KohaDates %].\r\nPlease return the item before the due date.\r\n\r\n[%- IF rule.past_due_fine_amount %]\r\n  If you fail to do so, you will be charged a fee of $[% rule.past_due_fine_amount | $Price %]\r\n[%- END %]\r\n[%- IF rule.past_due_restrict %]\r\n  If you fail to do so, you will be restricted from checking out new items.\r\n[%- END %]', 'email', 'default'),
             ('reserves', 'RECALL_PICKUP_PLUGIN', '', 'Recalled item ready for pickup', 0, 'Recalled item ready for pickup', 'Dear <<borrowers.firstname>> <<borrowers.surname>>,\r\n\r\nYou have a recall available for pickup as of <<reserves.waitingdate>>:\r\n\r\nTitle: <<biblio.title>>\r\nAuthor: <<biblio.author>>\r\nCopy: <<items.copynumber>>\r\nLocation: <<branches.branchname>>\r\n<<branches.branchaddress1>>\r\n<<branches.branchaddress2>>\r\n<<branches.branchaddress3>>\r\n<<branches.branchcity>> <<branches.branchzip>>\r\n', 'email', 'default');
-    });
+    }
+    );
 
-    $dbh->do(q{
+    $dbh->do(
+        q{
         UPDATE letter SET content = CONCAT( content, "\r\n--\r\nID: <<reserves.reserve_id>>.\r\n--" ) WHERE code = "HOLD" AND content NOT LIKE "%ID: <<reserves.reserve_id>>%"
-    });
+    }
+    );
 
     return 1;
 }
 
 sub update_syspref {
-    my ($self, $syspref_name, $data) = @_;
+    my ( $self, $syspref_name, $data ) = @_;
 
     my $name = $self->get_metadata->{name};
 
     my $syspref = C4::Context->preference($syspref_name);
-    $syspref =~ s|\n*/\* JS and CSS for $name Plugin.*End of JS and CSS for $name Plugin \*/||gs;
+    $syspref =~
+s|\n*/\* JS and CSS for $name Plugin.*End of JS and CSS for $name Plugin \*/||gs;
 
     my $template = $self->get_template( { file => "$syspref_name.tt" } );
     $template->param(%$data);
@@ -372,8 +456,7 @@ sub update_syspref {
     $template_output = qq|\n/* JS and CSS for $name Plugin 
    This JS was added automatically by installing the $name Plugin
    Please do not modify */|
-      . $template_output
-      . qq|/* End of JS and CSS for $name Plugin */|;
+      . $template_output . qq|/* End of JS and CSS for $name Plugin */|;
 
     $syspref .= $template_output;
     C4::Context->set_preference( $syspref_name, $syspref );
